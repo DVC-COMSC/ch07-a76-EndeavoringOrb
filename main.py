8fa6946a8c5d739694d17f870db0a5508f7c97b6
EndeavoringOrb <aaronbelikoff@gmail.com>
2022-10-11 09:14:56 -0700

    2nd commit

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 num1 = list(map(int, input().split()))
 num2 = list(map(int, input().split()))
 
-if len(num1) == len(num2) or len(num1) > len(num2):
+if len(num1) == len(num2) or len(num1) < len(num2):
     merged_result = num1 + num2
 else:
     merged_result = num2 + num1
